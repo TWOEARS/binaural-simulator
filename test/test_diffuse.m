2f904d9c46a292b702c26e188111904a36d8d35f
Fiete Winter <fiete.winter@uni-rostock.de>
2014-06-16 13:34:27 +0200

    update comments for directional diffuse source example

--- test/test_diffuse.m
+++ test/test_diffuse.m
@@ -5,35 +5,41 @@ close all
 
 test_startup; 
 
-%% input signals
-
-% input
-[input{1}] = ...
-  audioread(fullfile([database_path, 'stimuli/anechoic/instruments/anechoic_cello.wav']));
-% SSR works with single-precision
-input{1} = single(input{1}(:,1)./max(abs(input{1}(:,1))));
-
 %% processing parameters
-% Sources
-% source(1) = AudioSource(AudioSourceType.POINT,buffer.FIFO());
 
-angles = (-45:5:45);
-source(1) = AudioSource(...
-  AudioSourceType.PWD,buffer.Noise(1:length(angles)), ...
-  [cosd(angles);sind(angles); zeros(1, length(angles))]);
-source(1).set(...
-  'UnitFront', [0.0; 0.0; 1.0], ...
-  'UnitUp', [0.0; 1.0; 0.0]);
+% angles of diffuse source (CAUTION: propagation direction is defined)
+angles = (90:5:135);
+directions = [cosd(angles);sind(angles); zeros(1, length(angles))];
 
-source(1).AudioBuffer.set(...
+% ChannelMapping defines which input signal is mapped to an output of an 
+% audio source. If you're not sure, just use a 1-to-1 Mapping by
+% [1,2,3,..., N]
+ChannelMapping = 1:length(angles);
+
+% NoiseBuffer with white gaussian noise
+Buffer = buffer.Noise(ChannelMapping);
+Buffer.set(...
   'Variance', 0.02, ...
   'Mean', 0.0);
 
+% Source
+source = AudioSource(...        % define AudioSource with .. 
+  AudioSourceType.PWD, ...      % PlaneWaveDecomposition Type
+  Buffer, ...                   % Buffer as signal source;
+  directions);                  % directions of PWD
+
+% set orientation of the Source to the unit vectors of the world coordinate
+% system
+source.set(...
+  'UnitFront', [0.0; 0.0; 1.0], ...
+  'UnitUp', [0.0; 1.0; 0.0]);
+
 % Sinks/Head
 head = AudioSink(2);
+head.set('Position', [0; 0; 1.75]);  
+head.set('UnitFront', [1.0; 0.0; 0.0]);  % head is looking to positive x
 
 % HRIRs
-% construct
 hrir = DirectionalIR( ...
   fullfile([database_path, 'impulse_responses/qu_kemar_anechoic/QU_KEMAR_anechoic_3m.wav']));  
 hrir.plot();  % plot hrirs
@@ -42,34 +48,28 @@ hrir.plot();  % plot hrirs
 sim = SimulatorConvexRoom();  % simulator object
 
 sim.set(...
-  'SampleRate', 44100, ...           % sampling frequency
+  'SampleRate', 44100, ...        % sampling frequency
   'BlockSize', 2^12, ...          % blocksize
   'NumberOfThreads', 1, ...       % number of threads
-  'MaximumDelay', 0.1, ...        % maximum distance delay in seconds
+  'MaximumDelay', 0.0, ...        % maximum distance delay in seconds
   'Renderer', @ssr_binaural, ...  % SSR rendering function (do not change this!)
-  'HRIRDataset', hrir, ...
-  'Sources', source, ...
-  'Sinks', head);
-%% initialization
+  'HRIRDataset', hrir, ...        % assign HRIR-Object to Simulator
+  'Sources', source, ...          % assign sources to Simulator
+  'Sinks', head);                 % assign sinks to Simulator
+%% processing
 sim.set('Init',true);
-
-%% static scene
-
-% head is looking to positive x
-head.set('Position', [0; 0; 1.75]);
-head.set('UnitFront', [1.0; 0.0; 0.0]);
-head.removeData();
 
 sim.set('Refresh',true);
 sim.draw();
 
+% 5 seconds processing
 for idx=1:ceil(5*sim.SampleRate/sim.BlockSize)
   sim.set('Process',true);
 end
 
 out = head.getData();
 out = out/max(abs(out(:))); % normalize
-audiowrite('out_static.wav',out,sim.SampleRate);
+audiowrite('out_diffuse.wav',out,sim.SampleRate);
 
 %% clean up
 sim.set('ShutDown',true);
