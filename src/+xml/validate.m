6f3a6e62bbb607c7e08c2cd865928320de9ad05d
Fiete Winter <fiete.winter@uni-rostock.de>
2014-06-17 18:59:30 +0200

    workaround for file paths in xml validation

--- src/+xml/validate.m
+++ src/+xml/validate.m
@@ -3,6 +3,9 @@ function validate(filename, schema)
   import javax.xml.transform.Source;
   import javax.xml.transform.stream.StreamSource;
   import javax.xml.validation.*;
+  
+  filename = which(filename);
+  schema = which(schema);
 
   factory = SchemaFactory.newInstance('http://www.w3.org/2001/XMLSchema');
   schemaLocation = File(schema);
