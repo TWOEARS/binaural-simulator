53e120d6fa7a2b7abbf2f6f38b8f26f895362827
Fiete Winter <fiete.winter@uni-rostock.de>
2014-06-18 11:46:59 +0200

    rename and move schema file

--- test/test_xml.m
+++ test/test_xml.m
@@ -5,7 +5,7 @@ test_startup;
 
 %% xml configuration
 
-xml.validate('test.xml','../src/+xml/TwoEars.xsd');
+xml.validate('test.xml','tef.xsd');
 
 theDoc = xmlread('test.xml');
 theNode = theDoc.getDocumentElement;
