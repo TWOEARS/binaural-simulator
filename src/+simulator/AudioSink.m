5bd19bcd1ddb60f09faf4d65a97484526d7feb2b
Fiete Winter <fiete.winter@uni-rostock.de>
2014-06-25 13:58:17 +0200

    doc for AudioSink

--- src/+simulator/AudioSink.m
+++ src/+simulator/AudioSink.m
@@ -1,9 +1,13 @@
 classdef AudioSink < simulator.buffer.FIFO & simulator.Object
-  %UNTITLED2 Summary of this class goes here
-  %   Detailed explanation goes here
+  % Class for sink-objects in audio scene
 
   methods
     function obj = AudioSink(channels)
+  % function obj = AudioSink(channels)
+  % constructor of AudioSink class
+  % 
+  % Parameters:
+  %   channels: number of input channels (2 for binaural) @type integer
       if nargin < 1
         channels = 1;
       end
