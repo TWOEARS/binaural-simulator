f56150083ff41c08c4b31faec37d1e1ad7864dfa
Fiete Winter <fiete.winter@uni-rostock.de>
2014-06-25 14:10:38 +0200

    doc for AudioSourceType

--- src/+simulator/AudioSourceType.m
+++ src/+simulator/AudioSourceType.m
@@ -1,9 +1,19 @@
-classdef AudioSourceType 
+classdef AudioSourceType
+  % Enumeration for audio source types
+  %
+  % Enumeration contains the following labels
+  %   - POINT for point source emitting spherical waves
+  %   - PLANE for plane waves
+  %   - DIRECT for a direct binaural input (e.g. binaural recordings)
+  %   - PWD for a distributed plane wave containing plane waves arriving
+  %   from different directions
+  %
+  % See also: simulator.AudioSource
   enumeration
-      POINT,
-      PLANE,
-      DIRECT,
-      PWD
-  end  
+    POINT,
+    PLANE,
+    DIRECT,
+    PWD
+  end
 end
 
