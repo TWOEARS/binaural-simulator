46a9deb858bc80e39f199e6399dabcfa87460764
Fiete Winter <fiete.winter@uni-rostock.de>
2014-06-27 13:57:55 +0200

    if file does not exists in db, local file is assumed

--- src/+xml/getfile.m
+++ src/+xml/getfile.m
@@ -1,6 +1,14 @@
-function s = getfile(s)
+function filename = getfile(filename)
   if exist('dbgetfile', 'file') == 2
-    s = dbgetfile(s);
+    s = filename;
+    filename = dbgetfile(filename);
+    try
+      isargfile(filename);
+    catch
+      warning('file not found in database (%s). Assuming local file.', ...
+        s);
+      filename = s;
+    end
   else
     xml.nodbfound();
   end
