7817ed398f2cbe64c950bba271643d79c31a19cf
Fiete Winter <fiete.winter@uni-rostock.de>
2014-08-11 15:16:50 +0200

    modify RobotInterface for better documentation

--- src/+simulator/RobotInterface.m
+++ src/+simulator/RobotInterface.m
@@ -13,7 +13,7 @@ classdef (Abstract) RobotInterface < hgsetget
       %
       % rotate head either about (mode='relative') or to (mode='absolute')
       
-      isargscalar(angleDeg);      
+      isargscalar(angleDeg);
       if (nargin < 3)
         mode = 'relative';
       else
@@ -27,47 +27,49 @@ classdef (Abstract) RobotInterface < hgsetget
           obj.rotateHeadAbsolute(angleDeg);
         otherwise
           error('mode (%s) not supported', mode);
-      end      
+      end
     end
-  end  
-  methods (Abstract, Access=protected)
-    rotateHeadRelative(obj, angleIncDeg)
-      % function rotateHeadRelative(obj, angleIncDeg)
-      % rotate about an incremental angle in degrees
-      %
-      % Parameters:
-      %   angleIncDeg: angle increment in degree @type double
-      %
-      % negative angle result in rotation to the right
-      % positive angle result in rotation to the left
-    rotateHeadAbsolute(obj, angleDeg)
-      % function rotateHeadRelative(obj, angleIncDeg)
-      % rotate about an incremental angle in degrees
-      %
-      % Parameters:
-      %   angleDeg: absolute angle in degree @type double
-      %
-      % look directions:
-      %   0/ 360 degree = positive x-axis
-      %  90/-270 degrees = positive y-axis
-      % 180/-180 degrees = negative x-axis
-      % 270/- 90 degrees = negative y-axis
   end
   %% signal acquisition
-  methods (Abstract) 
+  methods (Abstract)
     [sig, timeIncSec, timeIncSamples] = getSignal(obj, timeIncSec)
-      % function [sig, timeIncSec, timeIncSamples] = getSignal(obj, timeIncSec)
-      % get binaural of specified length
-      %
-      % Parameters:
-      %   timeIncSec: length of signal in seconds @type double
-      %
-      % Return Values:
-      %   timeIncSec: length of signal in seconds @type double
-      %   timeIncSamples: length of signal in samples @type integer    
-      %
-      % Due to the frame-wise processing length of the output signal can
-      % vary from specified signal length. This is indicated by the 2nd and
-      % 3rd return value.
-  end 
+    % function [sig, timeIncSec, timeIncSamples] = getSignal(obj, timeIncSec)
+    % get binaural of specified length
+    %
+    % Parameters:
+    %   timeIncSec: length of signal in seconds @type double
+    %
+    % Return Values:
+    %   timeIncSec: length of signal in seconds @type double
+    %   timeIncSamples: length of signal in samples @type integer
+    %
+    % Due to the frame-wise processing length of the output signal can
+    % vary from specified signal length. This is indicated by the 2nd and
+    % 3rd return value.
+  end
+  methods (Abstract, Access=protected)
+    rotateHeadAbsolute(obj, angleDeg)
+    % function rotateHeadAbsolute(obj, angleDeg)
+    % rotate about an incremental angle in degrees
+    %
+    % Parameters:
+    %   angleDeg: absolute angle in degree @type double
+    %
+    % look directions:
+    %   0/ 360 degree = positive x-axis
+    %  90/-270 degrees = positive y-axis
+    % 180/-180 degrees = negative x-axis
+    % 270/- 90 degrees = negative y-axis
+  end
+  methods (Abstract, Access=protected)    
+    rotateHeadRelative(obj, angleIncDeg)
+    % function rotateHeadRelative(obj, angleIncDeg)
+    % rotate about an incremental angle in degrees
+    %
+    % Parameters:
+    %   angleIncDeg: angle increment in degree @type double
+    %
+    % negative angle result in rotation to the right
+    % positive angle result in rotation to the left
+  end
 end
