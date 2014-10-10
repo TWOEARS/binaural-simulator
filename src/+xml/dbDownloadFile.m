0848ff38486d84cf79f4c38c6992673b5af6962a
Fiete Winter <fiete.winter@uni-rostock.de>
2014-10-10 09:26:33 +0200

    fix bugs for WIN64 related to slashes and backslashes in filenames

--- src/+xml/dbDownloadFile.m
+++ src/+xml/dbDownloadFile.m
@@ -2,28 +2,36 @@ function outfile = dbDownloadFile(filename, outfile)
 % download file from remote database
 %
 % Parameters:
-%   filename: filename @type char[]
+%   filename: filename relative to root directory of the database @type char[]
 %   outfile: relative or absolute filename where download should be saved, optional @type char[]
 %
 % Return values:
-%   outfile: absolute filename
+%   outfile: absolute name of downloaded file
 %
 % Download file specified by filename relative to root directory of the remote
-% database. The root directory is defined via xml.dbURL().
+% database. The root directory is defined via xml.dbURL(). If no output
+% file is specified the file will relative to the temporary directory. The
+% temporary is via xml.dbTmp();
 %
-% See also: xml.dbGetFile xml.dbURL
+% See also: xml.dbGetFile xml.dbURL xml.getTmp
 
-if nargin < 2
-  dirs = regexp(filename, filesep, 'split');
+% split up filename into directories
+[dirs, sdx] = regexp(filename, '(\\|\/)', 'split', 'start');
 
+filename(sdx) = '/';  % replace backslashes with slashed for url
+url = [xml.dbURL(), '/', filename];
+
+% create directories if necessary
+if nargin < 2  
   dir_path = xml.dbTmp();
   for idx=1:length(dirs)-1
     dir_path = [dir_path, filesep, dirs{idx}];
     [~, ~] = mkdir(dir_path);
   end
-  outfile = [dir_path, filesep, dirs{end}];
+  outfile = fullfile(dir_path, dirs{end});
 end
-url = fullfile(xml.dbURL(), filename);
+
+% start download
 fprintf('Downloading file %s\n', url);
 [~, status] = urlwrite(url, outfile);
 
