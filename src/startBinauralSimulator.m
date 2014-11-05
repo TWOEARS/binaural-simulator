79bc924729da691424ae4411d675314e2e20bc16
Fiete Winter <fiete.winter@uni-rostock.de>
2014-11-05 16:09:17 +0100

    workaround for fftw related segfault

--- src/startBinauralSimulator.m
+++ src/startBinauralSimulator.m
@@ -2,6 +2,9 @@
 % 'BLAS loading error: dlopen: cannot load any more object with static TLS'
 ones(10)*ones(10);
 
+% Workaround for Bug related to fftw library
+fftw('swisdom');
+
 basePath = [fileparts(mfilename('fullpath')) filesep];
 
 addpath([basePath 'mex']);
