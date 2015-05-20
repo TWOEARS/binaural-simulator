6f8413e27daf659d4ee002c31126bec52aebb83a
Fiete Winter <fiete.winter@uni-rostock.de>
2015-05-20 10:53:22 +0200

    parse TwoEarsPaths.xml at startup to get path to local database

--- src/startBinauralSimulator.m
+++ src/startBinauralSimulator.m
@@ -1,13 +1,45 @@
-% Workaround for Bug in Matlab R2012b and higher
+%% workaround for bug in Matlab R2012b and higher
 % 'BLAS loading error: dlopen: cannot load any more object with static TLS'
 ones(10)*ones(10);
 
-% Workaround for Bug related to fftw library
+%% workaround for bug related to fftw library
 fftw('swisdom');
 
+%% search for TwoEarsPaths.xml and get path to local database
+warn = false;
+
+if exist('TwoEarsPaths.xml', 'file') == 2
+  docNode = xmlread('TwoEarsPaths.xml');
+  eleList = docNode.getDocumentElement.getElementsByTagName('data');
+  switch eleList.getLength
+    case 0
+      warning(['%s: Could not find any entry for ''data'' in the ', ...
+        '''TwoEarsPaths.xml''.'], upper(mfilename));
+      warn = true;
+    case 1
+      xml.dbPath( char( eleList.item(0).getFirstChild.getData ) );
+    otherwise
+      warning(['%s: Found more than one entry for ''data'' in the ', ...
+      '''TwoEarsPaths.xml''.'], upper(mfilename));
+      warn = true;
+  end
+else
+  warning('%s: Could not find any file named ''TwoEarsPaths.xml''', ...
+    upper(mfilename));
+  warn = true;
+end
+
+if warn
+  warning(['%s: Continuing without setting the path of the local ', ...
+    'database. Use xml.dbPath(''awesome/string/for/the/path/'') to ', ...
+    'configure the path manually, if necessary!'], upper(mfilename));
+end
+
+clear docNode eleList warn;  % Clear used variables
+
+%% add necessary paths
 basePath = [fileparts(mfilename('fullpath')) filesep];
 
 addpath([basePath 'mex']);
 
-% Clear used variables
-clear basePath;
+clear basePath;  % Clear used variables
