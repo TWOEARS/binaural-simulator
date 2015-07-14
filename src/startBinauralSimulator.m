94b5d380dc195ff89595dc77a85359583dca9aed
Fiete Winter <fiete.winter@uni-rostock.de>
2015-07-14 14:48:36 +0200

    remove some warnings

--- src/startBinauralSimulator.m
+++ src/startBinauralSimulator.m
@@ -13,29 +13,15 @@ if exist('TwoEarsPaths.xml', 'file') == 2
   eleList = docNode.getDocumentElement.getElementsByTagName('data');
   switch eleList.getLength
     case 0
-      warning(['%s: Could not find any entry for ''data'' in the ', ...
-        '''TwoEarsPaths.xml''.'], upper(mfilename));
-      warn = true;
     case 1
       xml.dbPath( char( eleList.item(0).getFirstChild.getData ) );
     otherwise
       warning(['%s: Found more than one entry for ''data'' in the ', ...
       '''TwoEarsPaths.xml''.'], upper(mfilename));
-      warn = true;
   end
-else
-  warning('%s: Could not find any file named ''TwoEarsPaths.xml''', ...
-    upper(mfilename));
-  warn = true;
 end
 
-if warn
-  warning(['%s: Continuing without setting the path of the local ', ...
-    'database. Use xml.dbPath(''awesome/string/for/the/path/'') to ', ...
-    'configure the path manually, if necessary!'], upper(mfilename));
-end
-
-clear docNode eleList warn;  % Clear used variables
+clear docNode eleList % Clear used variables
 
 %% add necessary paths
 basePath = [fileparts(mfilename('fullpath')) filesep];
