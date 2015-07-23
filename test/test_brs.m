1aee2135f51a8c54327627d18ffd5a2118826ba2
Fiete Winter <fiete.winter@uni-rostock.de>
2015-07-23 16:56:05 +0200

    read limits of headrotation from BRIR file

--- test/test_brs.m
+++ test/test_brs.m
@@ -10,14 +10,41 @@ sim.set('Init',true);
 
 %% static scene, dynamic head
 
-% head should rotate about 170 degree to the right with 20 degrees per second
-sim.Sinks.setDynamic('UnitX', 'Velocity', 20);
-sim.Sinks.set('UnitX', [cosd(85); sind(85); 0]);
+% brirs dataset limits head orientation from 0 to 180 degrees
 
+sim.Sinks.OrientationXY  % initial head orientation is 180 degrees
+
+% this should work ( head orientation still at 180 degrees )
+sim.rotateHead(180, 'relative'); 
+while sim.Time < 1.5
+  sim.set('Refresh',true);  % refresh all objects
+  sim.set('Process',true);
+end
+sim.Sinks.OrientationXY
+
+% this should work ( head orientation now at 0 degree )
+sim.rotateHead(-180, 'relative'); 
+while sim.Time < 3
+  sim.set('Refresh',true);  % refresh all objects
+  sim.set('Process',true);
+end
+sim.Sinks.OrientationXY
+
+% this should not work ( head orientation still at 0 degree  )
+sim.rotateHead(-90, 'absolute'); 
+while sim.Time < 4.5
+  sim.set('Refresh',true);  % refresh all objects
+  sim.set('Process',true);
+end
+sim.Sinks.OrientationXY
+
+% this should work again ( head orientation now at 90 degree  )
+sim.rotateHead(90, 'absolute'); 
 while ~sim.isFinished()
   sim.set('Refresh',true);  % refresh all objects
   sim.set('Process',true);
 end
+sim.Sinks.OrientationXY
 
 % save file
 sim.Sinks.saveFile('out_brs.wav',sim.SampleRate);
